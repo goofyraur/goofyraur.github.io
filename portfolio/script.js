f3578ef2f62f3c96800155aea10a92a7198bfd63
goofyraur <129874599+goofyraur@users.noreply.github.com>
2023-10-21 12:29:13 -0400

    Create script.js

---
+++ portfolio/script.js
@@ -0,0 +1,50 @@
+const header = document.getElementById('header')
+
+const startColor = [100, 255, 100, 0.4]
+const endColor = [40, 40, 40, 1]
+
+function lerpColor(alpha) {
+  const r = (endColor[0] - startColor[0]) * alpha + startColor[0]
+  const g = (endColor[1] - startColor[1]) * alpha + startColor[1]
+  const b = (endColor[2] - startColor[2]) * alpha + startColor[2]
+  const a = (endColor[3] - startColor[3]) * alpha + startColor[3]
+
+  return [r, g, b, a]
+}
+
+function getAlpha() {
+  const rect = header.getBoundingClientRect()
+  const dist = rect.top * -1 + 50
+  let a = 0
+  if (dist < 0) {
+    a = 0
+  } else if (dist > rect.height - 50) {
+    a = 1
+  } else {
+    a = dist / (rect.height - 50)
+  }
+
+  return a
+}
+
+function fixColor() {
+  const alpha = getAlpha()
+  const color = lerpColor(alpha)
+  header.style.backgroundColor = `rgba(${color[0]}, ${color[1]}, ${color[2]}, ${color[3]})`
+  header.style.backdropFilter = `blur(${(15 - 2) * alpha + 2}px)`
+}
+
+window.addEventListener("scroll", fixColor)
+window.addEventListener("resize", fixColor)
+
+const navButtons = Array.from(document.querySelectorAll('nav button'))
+for (const key in navButtons) {
+	navButtons[key].addEventListener('click', () => {
+  	const heading = document.getElementById('heading-' + navButtons[key].innerText.replaceAll(' ', '').toLowerCase())
+    if (heading) {
+    	window.scrollTo({top: heading.getBoundingClientRect().top + window.scrollY - 50, behavior: 'smooth'})
+    } else {
+    	window.scrollTo({ top: 0, behavior: 'smooth' })
+    }
+  })
+}
